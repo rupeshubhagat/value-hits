d8bb7aa1ffe8b347d3c93a6228f48a9cd8874d74
rupeshubhagat <bhagat.r29@gmail.com>
2020-07-13 21:20:12 +0530

    Worked on header and footer sections

---
+++ app/js/main.js
@@ -0,0 +1,12 @@
+// $(document).ready(function() {
+//     // jQuery methods go here...
+//     $(document).resize(function() {
+//         var screen = $(window);
+
+//         if (screen.width < 768) {
+//             $(".style-change").removeClass("style-change");
+//             $(".style-change").addClass("--common-style-top");
+//         }
+//     });
+
+// });
