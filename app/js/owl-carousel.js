961ea08d4c05b7f16060aa08ed7559b687b3829d
rupeshubhagat <bhagat.r29@gmail.com>
2020-07-15 21:23:49 +0530

    Worked on Banner section with responsive and client section with responsive

---
+++ app/js/owl-carousel.js
@@ -0,0 +1,52 @@
+$(document).ready(function() {
+    $('#banner-carousel').owlCarousel({
+        loop: true,
+        margin: 10,
+        responsiveClass: true,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: false
+            },
+            600: {
+                items: 1,
+                nav: false
+            },
+            1000: {
+                items: 1,
+                nav: false,
+                loop: true
+            }
+        }
+    })
+
+    $('#client-carousel').owlCarousel({
+        center: true,
+        items: 2,
+        dots: false,
+        loop: true,
+        margin: 10,
+        autoplay: true,
+        autoplayTimeout: 3000,
+        autoplayHoverPause: true,
+        responsive: {
+            0: {
+                items: 2,
+                nav: false
+            },
+            600: {
+                items: 3,
+                nav: false
+            },
+            1000: {
+                items: 4,
+                nav: false,
+                loop: true
+            }
+        }
+    })
+
+});
