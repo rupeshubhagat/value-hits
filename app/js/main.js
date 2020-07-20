233e0de01f3050353532038146e3c6a95ef1f306
rupeshubhagat <bhagat.r29@gmail.com>
2020-07-20 21:30:04 +0530

    working on seo pages

--- app/js/main.js
+++ app/js/main.js
@@ -10,4 +10,37 @@ $(document).ready(function() {
         $(".after-seo-btn").addClass("active-btn");
         $(".before-seo-btn").removeClass("active-btn");
     });
+
+    $(".seo-btn-bronze").click(function() {
+        $(".seo-btn-bronze").addClass("seo-btn-active");
+        $(".seo-btn-bronze-right").addClass("seo-btn-active");
+        $(".seo-btn-silver,.seo-btn-silver-right").removeClass("seo-btn-active");
+        $(".seo-btn-gold,.seo-btn-gold-right").removeClass("seo-btn-active");
+    });
+
+    $(".seo-btn-silver").click(function() {
+        $(".seo-btn-silver").addClass("seo-btn-active");
+        $(".seo-btn-silver-right").addClass("seo-btn-active");
+
+        $(".seo-btn-bronze,.seo-btn-bronze-right").removeClass("seo-btn-active");
+        $(".seo-btn-gold,.seo-btn-gold-right").removeClass("seo-btn-active");
+
+
+        // $(".seo-btn-silver,seo-btn-silver-right").removeClass("seo-btn-active");
+        // $(".seo-btn-gold,seo-btn-gold-right").removeClass("seo-btn-active");
+
+    });
+
+    $(".seo-btn-gold").click(function() {
+        $(".seo-btn-gold").addClass("seo-btn-active");
+        $(".seo-btn-gold-right").addClass("seo-btn-active");
+
+        $(".seo-btn-bronze,.seo-btn-bronze-right").removeClass("seo-btn-active");
+        $(".seo-btn-silver,.seo-btn-silver-right").removeClass("seo-btn-active");
+
+
+
+    });
+
+
 });
