2c00d017e7a6454f8e7250f1581513cd92356a3e
rupeshubhagat <bhagat.r29@gmail.com>
2020-07-21 20:54:51 +0530

    Worked on seo pages and started case study page

--- app/js/main.js
+++ app/js/main.js
@@ -14,16 +14,22 @@ $(document).ready(function() {
     $(".seo-btn-bronze").click(function() {
         $(".seo-btn-bronze").addClass("seo-btn-active");
         $(".seo-btn-bronze-right").addClass("seo-btn-active");
+        $(".seo-bronze-bg").addClass("seo-btn-bg");
+
         $(".seo-btn-silver,.seo-btn-silver-right").removeClass("seo-btn-active");
         $(".seo-btn-gold,.seo-btn-gold-right").removeClass("seo-btn-active");
+        $(".seo-gold-bg,.seo-silver-bg").removeClass("seo-btn-bg");
+
     });
 
     $(".seo-btn-silver").click(function() {
         $(".seo-btn-silver").addClass("seo-btn-active");
         $(".seo-btn-silver-right").addClass("seo-btn-active");
+        $(".seo-silver-bg").addClass("seo-btn-bg");
 
         $(".seo-btn-bronze,.seo-btn-bronze-right").removeClass("seo-btn-active");
         $(".seo-btn-gold,.seo-btn-gold-right").removeClass("seo-btn-active");
+        $(".seo-gold-bg,.seo-bronze-bg").removeClass("seo-btn-bg");
 
 
         // $(".seo-btn-silver,seo-btn-silver-right").removeClass("seo-btn-active");
@@ -34,9 +40,13 @@ $(document).ready(function() {
     $(".seo-btn-gold").click(function() {
         $(".seo-btn-gold").addClass("seo-btn-active");
         $(".seo-btn-gold-right").addClass("seo-btn-active");
+        $(".seo-gold-bg").addClass("seo-btn-bg");
+
 
         $(".seo-btn-bronze,.seo-btn-bronze-right").removeClass("seo-btn-active");
         $(".seo-btn-silver,.seo-btn-silver-right").removeClass("seo-btn-active");
+        $(".seo-silver-bg,.seo-bronze-bg").removeClass("seo-btn-bg");
+
 
 
 
